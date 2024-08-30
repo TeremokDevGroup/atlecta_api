a172b0d7a9dee61a5988bcc14b7d841a6569b31c
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2024-08-30 20:39:34 +0700

    Fix fields and refactoring

--- src/sports/schemas.py
+++ src/sports/schemas.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+from typing import Optional
+from src.sports.models import Sport as SportModel
 
 
 class SportBase(BaseModel):
     name: str
+
+    class Config:
+        from_attributes = True
+        frozen = True
+
+    class Meta:
+        orm_model = SportModel
 
 
 class SportCreate(SportBase):
@@ -12,19 +21,25 @@ class SportCreate(SportBase):
 class Sport(SportBase):
     id: int
 
-    class Config:
-        from_attributes = True
-
     def __eq__(self, other):
         return self.name == other.name
 
 
 class SportObjectBase(BaseModel):
+    name: str
     x_coord: float
     y_coord: float
-    address: str
+    address: Optional[str] = None
 
-    tags: set[Sport]
+    # NOTE: It's actually should be a set() but I get 'is not hashable' error when I call .model_dump()
+    tags: list[SportBase]
+
+    @field_validator("tags")
+    def validate_tags(cls, value: list[SportBase]) -> list[SportBase]:
+        return list(set(value))
+
+    class Config:
+        from_attributes = True
 
 
 class SportObjectCreate(SportObjectBase):
@@ -33,6 +48,3 @@ class SportObjectCreate(SportObjectBase):
 
 class SportObject(SportObjectBase):
     id: int
-
-    class Config:
-        from_attributes = True
