0774f99c121c2e497a479bd580f1dd8d8d4578bd
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2024-09-09 22:04:04 +0700

    Created superuser depenency

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -33,3 +33,5 @@ fastapi_users = FastAPIUsers[User, uuid.UUID](
 )
 
 current_active_user = fastapi_users.current_user(active=True)
+current_superuser = fastapi_users.current_user(
+    active=True, superuser=True)
