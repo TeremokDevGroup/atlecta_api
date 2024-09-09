c82563c711d44c921f3c8d7f894926cc34de5f3e
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2024-09-09 20:44:12 +0700

    Created simple routes to check user auth

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,1 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from .models import User
+from .auth import current_active_user
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/greeting")
+async def greeting():
+    return {"message": "Hello World"}
+
+
+@router.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+
+# @router.get("/")
+# async def get_all_users_profiles():
+#     sports = await UserProfileSQLAlchemyService().get_all()
+#     return {"sports": "Users profiles here"}
