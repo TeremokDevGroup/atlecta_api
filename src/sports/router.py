f6c97c3f89161b293c621fd3c82c107a0025f593
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2024-11-27 17:30:32 +0700

    Renamed router to sports_router

--- src/sports/router.py
+++ src/sports/router.py
@@ -4,44 +4,44 @@ from fastapi import APIRouter, Depends
 from .services import SportObjectSQLAlchemyService, SportSQLAlchemyService
 from src.sports.schemas import SportCreate, SportObjectCreate
 
-router = APIRouter(
+sports_router = APIRouter(
     prefix="/sports",
     tags=["sports"],
     responses={404: {"description": "Not found"}},
 )
 
 
-@router.get("/")
+@sports_router.get("/")
 async def get_all_sports():
     sports = await SportSQLAlchemyService().get_all()
     return {"sports": sports}
 
 
-@router.get("/{sport_id}")
+@sports_router.get("/{sport_id}")
 async def get_sport_by_id(sport_id: int):
     sport = await SportSQLAlchemyService().get_by_id(id=sport_id)
     return {sport_id: sport}
 
 
-@router.post("/")
+@sports_router.post("/")
 async def add_sport(sport: Annotated[SportCreate, Depends(SportCreate)]):
     sport_id = await SportSQLAlchemyService().add(sport)
     return {"sport_id": sport_id}
 
 
-@router.get("/objects/")
+@sports_router.get("/objects/")
 async def get_all_sport_objects():
-    sports = await SportObjectSQLAlchemyService().get_all()
-    return {"sport_objects": sports}
+    sport_objects = await SportObjectSQLAlchemyService().get_all()
+    return sport_objects
 
 
-@router.get("/objects/{sport_object_id}/")
+@sports_router.get("/objects/{sport_object_id}/")
 async def get_sport_object_by_id(sport_object_id: int):
     sport_object = await SportObjectSQLAlchemyService().get_by_id(id=sport_object_id)
     return {"sport_object": sport_object}
 
 
-@router.post("/objects")
+@sports_router.post("/objects")
 async def add_sport_object(sport_object: Annotated[SportObjectCreate, Depends(SportObjectCreate)]):
     sport_object = await SportObjectSQLAlchemyService().add(sport_object)
-    return {"sport_object": sport_object}
+    return sport_object
