46019107640b91916fcf26550334c89cb96a37e7
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2025-05-06 15:09:12 +0700

    Renamed created_at to uploaded_at

--- src/sports/models.py
+++ src/sports/models.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
+
+import uuid
 from datetime import datetime
 from uuid import UUID
-import uuid
 
-from sqlalchemy import Column, ForeignKey, Numeric, String, Table, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, DateTime, ForeignKey, Numeric, String, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
 
 from src.database import Base
 
@@ -64,5 +65,5 @@ class SportObjectImage(Base):
 
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
     url: Mapped[str] = mapped_column(String(255), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(
+    uploaded_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now())
