19e36fdb1387e007b17555d45cf9003fee478316
zawarudo-tokiwo <dancewiththedead69@gmail.com>
2025-05-24 20:34:10 +0700

    Added connection pool

--- src/database.py
+++ src/database.py
@@ -1,4 +1,3 @@
-from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 import src.config as config
@@ -14,12 +13,19 @@ Base = declarative_base()
 metadata = Base.metadata
 
 # engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True,
+                             pool_size=90,  # Maximum number of persistent connections
+                             max_overflow=5,  # Maximum number of connections to create beyond pool_size
+                             pool_recycle=3600,  # Recycle connections after this many seconds
+                             pool_timeout=30,  # Seconds to wait for a connection before timing out
+                             # pool_pre_ping=True,  # Test connection before using it
+                             )
 
-async_session_maker = async_sessionmaker(
+
+async_session_factory = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
+    async with async_session_factory() as session:
         yield session
